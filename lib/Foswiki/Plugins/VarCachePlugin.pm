10a4effc943a6319c1675b2c9b8b4268a8a33f8c
AndrewJones <AndrewJones@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2011-05-05 11:12:10 +0000

    Item10713: tidy
    git-svn-id: http://svn.foswiki.org/trunk/VarCachePlugin@11634 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/VarCachePlugin.pm
+++ lib/Foswiki/Plugins/VarCachePlugin.pm
@@ -22,9 +22,9 @@ package Foswiki::Plugins::VarCachePlugin;
 
 # =========================
 use vars qw(
-        $VERSION $RELEASE $pluginName
-        $debug $paramMsg $NO_PREFS_IN_TOPIC
-    );
+  $VERSION $RELEASE $pluginName
+  $debug $paramMsg $NO_PREFS_IN_TOPIC
+);
 
 # This should always be $Rev$ so that Foswiki can determine the checked-in
 # status of the plugin. It is used by the build automation tools, so
@@ -36,68 +36,69 @@ $VERSION = '$Rev$';
 # of the version number in PLUGINDESCRIPTIONS.
 $RELEASE = '1.0';
 
-$pluginName = 'VarCachePlugin';  # Name of this Plugin
+$pluginName = 'VarCachePlugin';    # Name of this Plugin
 
 $NO_PREFS_IN_TOPIC = 1;
 
 # =========================
-sub initPlugin
-{
+sub initPlugin {
     my ( $topic, $web, $user, $installWeb ) = @_;
 
-    # check for Plugins.pm versions
-    if( $Foswiki::Plugins::VERSION < 1.024 ) {
-        Foswiki::Func::writeWarning( "Version mismatch between $pluginName and Plugins.pm" );
-        return 0;
-    }
-
     # Get plugin debug flag
-    $debug = Foswiki::Func::getPreferencesFlag( "VARCACHEPLUGIN_DEBUG" );
+    $debug = Foswiki::Func::getPreferencesFlag("VARCACHEPLUGIN_DEBUG");
 
     # Plugin correctly initialized
-    Foswiki::Func::writeDebug( "- Foswiki::Plugins::${pluginName}::initPlugin( $web.$topic ) is OK" ) if $debug;
+    Foswiki::Func::writeDebug(
+        "- Foswiki::Plugins::${pluginName}::initPlugin( $web.$topic ) is OK")
+      if $debug;
     return 1;
 }
 
 # =========================
-sub beforeCommonTagsHandler
-{
+sub beforeCommonTagsHandler {
 ### my ( $text, $topic, $web ) = @_;   # do not uncomment, use $_[0], $_[1]... instead
 
-    Foswiki::Func::writeDebug( "- ${pluginName}::beforeCommonTagsHandler( $_[2].$_[1] )" ) if $debug;
+    Foswiki::Func::writeDebug(
+        "- ${pluginName}::beforeCommonTagsHandler( $_[2].$_[1] )")
+      if $debug;
 
-    return unless( $_[0] =~ /%VARCACHE/ );
+    return unless ( $_[0] =~ /%VARCACHE/ );
 
     $_[0] =~ s/%VARCACHE{(.*?)}%/_handleVarCache( $_[2], $_[1], $1 )/ge;
 
-    $_[0] =~ s/^.*(%--VARCACHE\:read\:.*?--%).*$/$1/os; # remove all text if "read cache"
+    $_[0] =~ s/^.*(%--VARCACHE\:read\:.*?--%).*$/$1/os
+      ;    # remove all text if "read cache"
 }
 
 # =========================
-sub afterCommonTagsHandler
-{
+sub afterCommonTagsHandler {
 ### my ( $text, $topic, $web ) = @_;   # do not uncomment, use $_[0], $_[1]... instead
 
-    Foswiki::Func::writeDebug( "- ${pluginName}::afterCommonTagsHandler( $_[2].$_[1] )" ) if $debug;
+    Foswiki::Func::writeDebug(
+        "- ${pluginName}::afterCommonTagsHandler( $_[2].$_[1] )")
+      if $debug;
 
-    return unless( $_[0] =~ /%--VARCACHE\:/ );
+    return unless ( $_[0] =~ /%--VARCACHE\:/ );
 
-    if( $_[0] =~ /%--VARCACHE\:([a-z]+)\:?(.*?)--%/ ) {
+    if ( $_[0] =~ /%--VARCACHE\:([a-z]+)\:?(.*?)--%/ ) {
         my $save = ( $1 eq "save" );
         my $age = $2 || 0;
         my $cacheFilename = _cacheFileName( $_[2], $_[1], $save );
 
-        if( $save ) {
+        if ($save) {
+
             # update cache
             Foswiki::Func::saveFile( $cacheFilename, $_[0] );
             $msg = _formatMsg( $_[2], $_[1] );
             $_[0] =~ s/%--VARCACHE\:.*?--%/$msg/go;
 
-        } else {
+        }
+        else {
+
             # read cache
-            my $text = Foswiki::Func::readFile( $cacheFilename );
+            my $text = Foswiki::Func::readFile($cacheFilename);
             $msg = _formatMsg( $_[2], $_[1] );
-            $msg =~ s/\$age/_formatAge($age)/geo;
+            $msg  =~ s/\$age/_formatAge($age)/geo;
             $text =~ s/%--VARCACHE.*?--%/$msg/go;
             $_[0] = $text;
         }
@@ -105,13 +106,13 @@ sub afterCommonTagsHandler
 }
 
 # =========================
-sub _formatMsg
-{
+sub _formatMsg {
     my ( $theWeb, $theTopic ) = @_;
 
-    my $msg = $paramMsg; # FIXME: Global variable not reliable in mod_perl
+    my $msg = $paramMsg;    # FIXME: Global variable not reliable in mod_perl
     $msg =~ s|<nop>||go;
-    $msg =~ s|\$link|%SCRIPTURL%/view%SCRIPTSUFFIX%/%WEB%/%TOPIC%?varcache=refresh|go;
+    $msg =~
+      s|\$link|%SCRIPTURL%/view%SCRIPTSUFFIX%/%WEB%/%TOPIC%?varcache=refresh|go;
     $msg =~ s|%ATTACHURL%|%PUBURL%/$installWeb/$pluginName|go;
     $msg =~ s|%ATTACHURLPATH%|%PUBURLPATH%/$installWeb/$pluginName|go;
     $msg = Foswiki::Func::expandCommonVariables( $msg, $theTopic, $theWeb );
@@ -119,58 +120,65 @@ sub _formatMsg
 }
 
 # =========================
-sub _formatAge
-{
-    my ( $age ) = @_;
+sub _formatAge {
+    my ($age) = @_;
 
     my $unit = "hours";
-    if( $age > 24 ) {
+    if ( $age > 24 ) {
         $age /= 24;
         $unit = "days";
-    } elsif( $age < 1 ) {
+    }
+    elsif ( $age < 1 ) {
         $age *= 60;
         $unit = "min";
     }
-    if( $age >= 3 ) {
-        $age = int( $age );
+    if ( $age >= 3 ) {
+        $age = int($age);
         return "$age $unit";
     }
     return sprintf( "%1.1f $unit", $age );
 }
 
 # =========================
-sub _handleVarCache
-{
+sub _handleVarCache {
     my ( $theWeb, $theTopic, $theArgs ) = @_;
 
-    my $query = Foswiki::Func::getCgiQuery();
+    my $query  = Foswiki::Func::getCgiQuery();
     my $action = "check";
-    if( $query ) {
-        my $tmp = $query->param( 'varcache' ) || "";
-        if( $tmp eq "refresh" ) {
+    if ($query) {
+        my $tmp = $query->param('varcache') || "";
+        if ( $tmp eq "refresh" ) {
             $action = "refresh";
-        } else {
-            $action = "" if( grep{ !/^refresh$/ } $query->param );
+        }
+        else {
+            $action = "" if ( grep { !/^refresh$/ } $query->param );
         }
     }
 
-    if( $action eq "check" ) {
+    if ( $action eq "check" ) {
         my $filename = _cacheFileName( $theWeb, $theTopic, 0 );
-        if( -e $filename ) {
+        if ( -e $filename ) {
             my $now = time();
-            my $cacheTime = (stat $filename)[9] || 10000000000;
+            my $cacheTime = ( stat $filename )[9] || 10000000000;
+
             # CODE_SMELL: Assume file system for topics
             $filename = Foswiki::Func::getDataDir() . "/$theWeb/$theTopic.txt";
-            my $topicTime = (stat $filename)[9] || 10000000000;
-            my $refresh = Foswiki::Func::extractNameValuePair( $theArgs, "refresh" )
-                       || Foswiki::Func::getPreferencesValue( "VARCACHEPLUGIN_REFRESH" ) || 24;
+            my $topicTime = ( stat $filename )[9] || 10000000000;
+            my $refresh =
+                 Foswiki::Func::extractNameValuePair( $theArgs, "refresh" )
+              || Foswiki::Func::getPreferencesValue("VARCACHEPLUGIN_REFRESH")
+              || 24;
             $refresh *= 3600;
-            if( ( ( $refresh == 0 ) || ( $cacheTime >= $now - $refresh ) )
-             && ( $cacheTime >= $topicTime ) ) {
+            if (   ( ( $refresh == 0 ) || ( $cacheTime >= $now - $refresh ) )
+                && ( $cacheTime >= $topicTime ) )
+            {
+
                 # add marker for afterCommonTagsHandler to read cached file
-                $paramMsg = Foswiki::Func::extractNameValuePair( $theArgs, "cachemsg" )
-                         || Foswiki::Func::getPreferencesValue( "VARCACHEPLUGIN_CACHEMSG" )
-                         || 'This topic was cached $age ago ([[$link][refresh]])';
+                $paramMsg =
+                  Foswiki::Func::extractNameValuePair( $theArgs, "cachemsg" )
+                  || Foswiki::Func::getPreferencesValue(
+                    "VARCACHEPLUGIN_CACHEMSG")
+                  || 'This topic was cached $age ago ([[$link][refresh]])';
                 $cacheTime = sprintf( "%1.6f", ( $now - $cacheTime ) / 3600 );
                 return "%--VARCACHE\:read:$cacheTime--%";
             }
@@ -178,11 +186,13 @@ sub _handleVarCache
         $action = "refresh";
     }
 
-    if( $action eq "refresh" ) {
+    if ( $action eq "refresh" ) {
+
         # add marker for afterCommonTagsHandler to refresh cache file
-        $paramMsg = Foswiki::Func::extractNameValuePair( $theArgs, "updatemsg" )
-                 || Foswiki::Func::getPreferencesValue( "VARCACHEPLUGIN_UPDATEMSG" )
-                 || 'This topic is now cached ([[$link][refresh]])';
+        $paramMsg =
+             Foswiki::Func::extractNameValuePair( $theArgs, "updatemsg" )
+          || Foswiki::Func::getPreferencesValue("VARCACHEPLUGIN_UPDATEMSG")
+          || 'This topic is now cached ([[$link][refresh]])';
         return "%--VARCACHE\:save--%";
     }
 
@@ -191,11 +201,10 @@ sub _handleVarCache
 }
 
 # =========================
-sub _cacheFileName
-{
+sub _cacheFileName {
     my ( $web, $topic, $mkDir ) = @_;
 
-    my $dir = Foswiki::Func::getWorkArea( $pluginName );
+    my $dir = Foswiki::Func::getWorkArea($pluginName);
     return "$dir/$web\_$topic.txt";
 }
 
